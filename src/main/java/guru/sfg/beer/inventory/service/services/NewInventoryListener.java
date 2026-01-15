f7f2270c16d2d41603f05c3bb2ca8f566b58cbb8
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2026-01-15 08:38:07 +0100

    Lesson 226. Refactor Model to Common Package

--- src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import guru.sfg.beer.inventory.service.config.JmsConfig;
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
-import guru.sfg.common.events.NewInventoryEvent;
+import guru.sfg.brewery.model.events.NewInventoryEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
