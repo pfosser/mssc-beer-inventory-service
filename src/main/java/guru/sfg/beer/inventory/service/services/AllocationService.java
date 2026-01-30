1eb1c7d927fa9ef3c39a1e98c2fbb093f15a52af
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2026-01-30 08:55:28 +0100

    Lesson 231. Add Allocation Service

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,13 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+
+public interface AllocationService {
+
+	/**
+	 * @param beerOrder
+	 * @return {@code true} if we allocate everything, {@code false} if we are short
+	 *         on allocation
+	 */
+	Boolean allocateOrder(BeerOrderDto beerOrder);
+}
